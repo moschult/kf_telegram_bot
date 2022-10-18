48d02fb93f0097bf47612a50e9f124bb9a266740
Moritz Schult <moritzschult@gmail.com>
2022-10-18 21:46:42 +0200

    Add basic logging

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from telegram.ext import Updater
+import logging
+
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from telegram.ext import Updater
+
 import config.config as cfg
+
+curr_level = "Your current level is "
+curr_gold = "Your current gold amount is "
+curr_unspent = "You currently have "
 
 updater = Updater(token=cfg.API_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
@@ -16,20 +23,26 @@ def start(update: Update, context: CallbackContext):
                                                                     "/unspent to show your unspent skill points"
                              )
 
+
 def handleLevel(update: Update, context: CallbackContext):
+    logging.info(curr_level)
     level = None
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f'Your current level is {level}')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'{curr_level} {level}')
+
 
 def handleGold(update: Update, context: CallbackContext):
-        gold = None
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Your current gold amount is {gold}')
+    logging.info(curr_gold)
+    gold = None
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'{curr_gold} {gold}')
+
 
 def handleUnspent(update: Update, context: CallbackContext):
-        unspent = 0
-        if unspent > 0:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f'You currently have {unspent} skill points')
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=f"You don't have any unspent skill points")
+    logging.info(curr_unspent)
+    unspent = 0
+    if unspent > 0:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'{curr_unspent} {unspent} skill points')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"You don't have any unspent skill points")
 
 
 def freeTextHandler(update: Update, context: CallbackContext):
@@ -60,9 +73,24 @@ def registerHandlers():
     echo_handler = MessageHandler(Filters.text & (~Filters.command), freeTextHandler)
     dispatcher.add_handler(echo_handler)
 
+
 def startUpdater():
     updater.start_polling(1)
     updater.idle()
 
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Create Configuration')
+parser.add_argument('--log', type=str, help='Loglevel',
+                    default="WARNING")
+
+args = parser.parse_args()
+getattr(logging, args.log.upper())
+numeric_level = getattr(logging, args.log.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % args.log)
+logging.basicConfig(level=numeric_level)
+logging.info("Starting application")
 registerHandlers()
 startUpdater()
